6b3da8c10a162b2eb8f458e79d0a0f40a4eac05e
dazzlingtea <kitsnr08@gmail.com>
2024-06-12 11:00:55 +0900

    # react props1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ const App = () => {
 
     return (
       <>
-        <ExpenseItem />
+        <ExpenseItem title={"치킨 주문"} price={12000} date={new Date(2024, 6, 3)} />
+        <ExpenseItem title="족발 존맛" price={27000} date={new Date(2024, 6, 7)} />
+        <ExpenseItem title="헬스장 등록" price={110000} date={new Date(2024, 6, 12)} />
       </>
     );
 }
