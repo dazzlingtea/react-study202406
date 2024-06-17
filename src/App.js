8cebfaa098f521e503f2e6d0daeb44174afc3bb2
dazzlingtea <kitsnr08@gmail.com>
2024-06-17 12:25:48 +0900

    # useRef 기초

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, {useState} from 'react';
-import CourseInput from './components/CourseGoals/CourseInput';
-import CourseList from './components/CourseGoals/CourseList';
+import React, {useRef, useState} from 'react';
 import './App.css';
 import AddUsers from "./components/Users/AddUsers";
 import UserList from "./components/Users/UserList";
-import ErrorModal from "./components/UI/Modal/ErrorModal";
 
 const App = () => {
 
   const [userList, setUserList] = useState([]);
 
+  const count = useRef(1);
+
   const addUserHandler = user => {
+    console.log('count: ', count)
+    count.current++;
+    console.log('count.curr: ', count.current);
+
     console.log(user);
     // 상태값 바꾸기 위해서 카피해서 새 배열로 만들고 setter에 대입
 
