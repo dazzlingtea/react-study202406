ee5a4aa579245f6d2653811d3c9341f89d6ef84d
dazzlingtea <kitsnr08@gmail.com>
2024-06-18 09:16:01 +0900

    # Side Effect template 작성

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React, {useRef, useState} from 'react';
 import './App.css';
-import AddUsers from "./components/Users/AddUsers";
-import UserList from "./components/Users/UserList";
+import MainHeader from "./components/SideEffect/MainHeader";
+import Home from "./components/SideEffect/Home";
+
 
 const App = () => {
 
-  const [userList, setUserList] = useState([]);
-
-  const count = useRef(1);
-
-  const addUserHandler = user => {
-    console.log('count: ', count)
-    count.current++;
-    console.log('count.curr: ', count.current);
-
-    console.log(user);
-    // 상태값 바꾸기 위해서 카피해서 새 배열로 만들고 setter에 대입
-
-    setUserList(prev => [
-      ...prev,
-      {...user,
-        id: Math.random().toString()
-      }
-    ]);
-  };
 
   return (
     <>
-      <AddUsers onAddUser={addUserHandler} />
-      <UserList users={userList} />
+      <MainHeader />
+      <main>
+        <Home />
+        {/*<Login />*/}
+      </main>
+
     </>
   );
 };
