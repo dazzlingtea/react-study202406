841870043b69f5ca39a04d18d01b2590b0cef0e1
dazzlingtea <kitsnr08@gmail.com>
2024-06-18 09:29:55 +0900

    # 로그인 기능 동작

--- src/components/SideEffect/Login.js
+++ src/components/SideEffect/Login.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import styles from './Login.module.css';
 import Button from '../UI/Button';
 
-const Login = ({ onLogin }) => {
+const Login = ({onLogin}) => {
+
+  // 사용자가 입력한 이메일을 상태관리
   const [enteredEmail, setEnteredEmail] = useState('');
+  // 이메일 입력값이 정상인지 유무 확인
   const [emailIsValid, setEmailIsValid] = useState();
+  // 사용자가 입력한 패스워드를 상태관리
   const [enteredPassword, setEnteredPassword] = useState('');
+  // 패스워드 입력값이 정상인지 유무 확인
   const [passwordIsValid, setPasswordIsValid] = useState();
+  // 이메일, 패스워드가 둘다 정상인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
   const emailChangeHandler = (e) => {
@@ -72,7 +78,10 @@ const Login = ({ onLogin }) => {
           />
         </div>
         <div className={styles.actions}>
-          <Button type="submit" className={styles.btn} disabled={!formIsValid}>
+          <Button
+            type="submit"
+            className={styles.btn}
+            disabled={!formIsValid}>
             Login
           </Button>
         </div>
