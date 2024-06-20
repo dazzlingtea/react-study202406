861e38d1fbc12c04b5b1bbe00c112ce0069d47df
dazzlingtea <kitsnr08@gmail.com>
2024-06-20 10:44:38 +0900

    # Ref 도전과제 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Player from "./components/Ref/Player";
+import TimeChallenge from "./components/Ref/TimeChallenge";
 
 
 const App = () => {
@@ -9,7 +10,12 @@ const App = () => {
   return (
     <>
       <Player />
-      <div id="challengers"></div>
+      <div id="challengers">
+        <TimeChallenge title='Easy' targetTime={1}/>
+        <TimeChallenge title='Not Easy' targetTime={5}/>
+        <TimeChallenge title='Getting Tough' targetTime={10}/>
+        <TimeChallenge title='Props Only' targetTime={15}/>
+      </div>
     </>
   );
 };
