29e6e0c668737decc975889bba64709fcd96c058
dazzlingtea <kitsnr08@gmail.com>
2024-06-20 12:22:38 +0900

    # Ref setInterval 처리

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -2,56 +2,59 @@ import React, {useRef, useState} from 'react';
 import ResultModal from "./ResultModal";
 
 
-const TimerChallenge = ({ title, targetTime }) => {
+const TimerChallenge = ({title, targetTime}) => {
 
   // timer를 ref 변수로 관리
   const timer = useRef();
 
-  // 타이머가 시작되었는지를 확인하는 상태값
-  const [timerStarted, setTimerStarted] = useState(false);
-
-  // 타겟시간이 종료되었는지 여부
-  const [timerExpired, setTimerExpired] = useState(false);
-
   // 자식컴포넌트 ResultModal에 있는 dialog태그의 참조를 만듬
   const dialog = useRef();
 
-  const startHandler = e => {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      // modal open !!
-      dialog.current.showModal();
-    }, targetTime * 1000);
+  // stop을 눌렀을 때 남은 시간을 상태값으로 관리
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-    // setTimerStarted(prevState => !prevState);
-    setTimerStarted(true);
+  // start stop 활성화 조건
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+
+  // 시간이 타임오버되었을 때 처리
+  if(timeRemaining < 0) {
+    clearInterval(timer.current);
+    dialog.current.showModal();
+  }
+
+
+  const startHandler = e => {
+    timer.current = setInterval(() => {
+      setTimeRemaining(prevTime => prevTime - 10);
+    }, 10);
   };
 
-  // 전역변수로 timer 설정시 5초 -> 1초 -> 1초 -> 5초 연속클릭해보면
-  // 5초짜리 timer가 정상작동하지 않는 이유는
-  // 4개의 TimeChallenge 컴포넌트가 1개의 timer를 공유하여
-  // 처음 5초짜리 timer가 1초짜리에 의해 덮어씌워지기 때문
   const stopHandler = e => {
 
     clearTimeout(timer.current);
-
+    dialog.current.showModal();
   };
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        result="lost"/>
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
-          {targetTime} second{targetTime > 1 ? 's': ''}
+          {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-          <button onClick={timerStarted ? stopHandler : startHandler}>
-            { timerStarted ? 'Stop' : 'Start'} Challenge
+          <button onClick={timerIsActive ? stopHandler : startHandler}>
+            {timerIsActive ? 'Stop' : 'Start'} Challenge
           </button>
         </p>
-        <p className={timerStarted ? 'active': undefined}>
-          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        <p className={timerIsActive ? 'active' : undefined}>
+          {timerIsActive ? 'Time is running...' : 'Timer inactive'}
         </p>
       </section>
     </>
