2547022591a861e747bd74aac0b1150aeed31394
dazzlingtea <kitsnr08@gmail.com>
2024-06-21 09:58:05 +0900

    # Food Input 컴포넌트 속성값 스프레드

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
-const Input = ({label}) => {
+const Input = ({ label, inputAttr }) => {
   return (
     <div className={styles.input}>
       <lable>{label}</lable>
-      <input />
+      <input {...inputAttr} />
     </div>
   );
 };
