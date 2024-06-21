9f21e6d77c1f77d562c469fc9023a8159d706805
dazzlingtea <kitsnr08@gmail.com>
2024-06-21 10:49:51 +0900

    # Food CartContext 정의

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Header from "./components/Food/Layout/Header";
 import Meals from "./components/Food/Meals/Meals";
 import Cart from "./components/Food/Cart/Cart";
-
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
 
   return (
-    <>
-      {/*<Cart />*/}
+    <CartProvider>
+      <Cart />
       <Header />
       <div id="main">
         <Meals />
       </div>
-    </>
+    </CartProvider>
   );
 };
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartContext from "./cart-context";
+
+
+const CartProvider = ({children}) => {
+  return (
+    <CartContext.Provider>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
