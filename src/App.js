f848d97be7e20bae921842e3197caceb84b417ae
dazzlingtea <kitsnr08@gmail.com>
2024-07-02 11:21:41 +0900

    # 라우팅 - 중첩 라우터

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import Home from "./components/RouteExample/pages/Home";
 import Products from "./components/RouteExample/pages/Products";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RootLayout from "./components/RouteExample/layout/RootLayout";
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/products', element: <Products /> }
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Products /> }
+    // 메뉴에 따라 paht 추가하면 됨
+    ]
+  },
+
 ]);
 
 const App = () => {
