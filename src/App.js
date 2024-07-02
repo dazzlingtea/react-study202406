6d9cb9ecb37ce9228034f7bf369b734cd816bf08
dazzlingtea <kitsnr08@gmail.com>
2024-07-02 10:41:52 +0900

    # 라우팅 - 세팅

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, {useState} from 'react';
-import Header from "./components/Food/Layout/Header";
-import Meals from "./components/Food/Meals/Meals";
-import Cart from "./components/Food/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import React from 'react';
 
 const App = () => {
 
-  // 장바구니 모달을 열고 닫는 상태변수
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  // 모달 열어주는 핸들러
-  const showCartHandler = () => setCartIsShown(true);
-
-  // 모달을 닫아주는 핸들러
-  const hideCartHandler = () => setCartIsShown(false);
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <div id="main">
-        <Meals />
-      </div>
-    </CartProvider>
+    <div>App</div>
   );
 };
 
