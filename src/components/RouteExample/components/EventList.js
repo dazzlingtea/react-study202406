516b06293d6c8787d0cdcea46e0b9ed0fd8fc406
dazzlingtea <kitsnr08@gmail.com>
2024-07-03 11:35:34 +0900

    # 리액트-서버 연동 : loader 분리 및 export

--- src/components/RouteExample/components/EventList.js
+++ src/components/RouteExample/components/EventList.js
@@ -17,11 +17,11 @@ const EventList = ({ eventList }) => {
         {
           eventList.map(ev => (
             <li key={ev.id} className={item}>
-              <Link to={ev.id.toString()}>
-                <img src={ev.image} alt={ev.title}/>
+              <Link to={ev.id}>
+                <img src={ev['img-url']} alt={ev.title}/>
                 <div className={content}>
                   <h2>{ev.title}</h2>
-                  <time>{ev.date}</time>
+                  <time>{ev.startDate}</time>
                 </div>
               </Link>
             </li>
