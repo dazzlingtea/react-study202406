ff75aa05d2a9bf6c7883d49f0b9c7b867876c73a
dazzlingtea <kitsnr08@gmail.com>
2024-07-04 11:54:11 +0900

    # 리액트-서버 연동 : 수정 날짜 input 렌더링

--- src/components/RouteExample/components/EventForm.js
+++ src/components/RouteExample/components/EventForm.js
@@ -3,6 +3,7 @@ import React from "react";
 import styles from './EventForm.module.scss';
 import {useParams, useNavigate} from "react-router-dom";
 
+
 const EventForm = ({method, event={}}) => {
 
   const {
@@ -21,11 +22,13 @@ const EventForm = ({method, event={}}) => {
     const [yearPart, monthDayPart] = date.split('년 ');
     const [monthPart, dayPart] = monthDayPart.split("월 ");
     const day = dayPart.replace('일', '');
-    // console.log('date: ', {yearPart, monthPart, day})
+    console.log('date: ', {yearPart, monthPart, day})
     return `${yearPart}-${monthPart}-${day}`;
   };
+
   let formatDate;
-  if(event.date) {
+  if(event['start-date']) {
+    console.log('event.date 있으면: ',event.date);
     formatDate = convertDateFormat(date);
   }
 
@@ -38,8 +41,37 @@ const EventForm = ({method, event={}}) => {
     navigate('..'); // 상대경로 잘 작동
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // input 입력값 가져오기
+    const formData = new FormData(e.target);
+    // console.log('form: ', formData.get('title'));
+
+    // 서버에 보낼 데이터
+    const payload = {
+      title: formData.get('title'),
+      desc: formData.get('description') ,
+      imageUrl: formData.get('image') ,
+      beginDate: formData.get('date')
+    };
+    // console.log('payload: ', payload);
+
+    // 서버로 페칭
+    (async () => {
+      const response = await fetch(`http://localhost:8282/events`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload)
+      });
+      navigate('/events');
+    })();
+
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler} noValidate>
       <p>
         <label htmlFor="title">Title</label>
         <input id="title" type="text" name="title"
