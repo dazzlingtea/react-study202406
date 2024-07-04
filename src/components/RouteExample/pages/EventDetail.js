d9c0c957f3b193c66f520116e9f4ecde8561a3c7
dazzlingtea <kitsnr08@gmail.com>
2024-07-04 09:57:02 +0900

    # 리액트-서버 연동 : 중간과정

--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLoaderData, useParams} from "react-router-dom";
 
 const EventDetail = () => {
 
   const { eventId: id} = useParams();
+  const [ev, setEv] = useState({});
 
-  // const data = useLoaderData(); 사용불가
-  // console.log('loader data: ',data);
+
+
+  useEffect(()=>{
+    (async ()=>{
+      const response = await fetch(`http://localhost:8282/events/${id}`);
+
+      const json = await response.json();
+      setEv(json);
+
+    })();
+  },[]);
+
 
   return (
     <>
       <h1>EventDetail Page</h1>
-      <p>Event ID: {id}</p>
+      <p>{ev.title}</p>
+      <p>{ev.desc}</p>
+      <p>{ev['start-date']}</p>
+      }
     </>
   );
 };
