23e6d23e1ec789099d0fbe9109165f6a324be82f
dazzlingtea <kitsnr08@gmail.com>
2024-07-04 10:22:24 +0900

    # 리액트-서버 연동 : 컴포넌트 밖에서 params

--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -4,21 +4,20 @@ import EventItem from "../components/EventItem";
 
 const EventDetail = () => {
 
-  const { eventId: id} = useParams();
-  const [ev, setEv] = useState({});
+  // const { eventId: id} = useParams();
+  // const [ev, setEv] = useState({});
+  //
+  // useEffect(()=>{
+  //   (async ()=>{
+  //     const response = await fetch(`http://localhost:8282/events/${id}`);
+  //     // 예외처리 필요한데 생략
+  //     const json = await response.json();
+  //     setEv(json);
+  //
+  //   })();
+  // },[]);
 
-
-
-  useEffect(()=>{
-    (async ()=>{
-      const response = await fetch(`http://localhost:8282/events/${id}`);
-      // 예외처리 필요한데 생략
-      const json = await response.json();
-      setEv(json);
-
-    })();
-  },[]);
-
+  const ev = useLoaderData();
 
   return (
     <EventItem event={ev} />
@@ -26,3 +25,18 @@ const EventDetail = () => {
 };
 
 export default EventDetail;
+
+export const loader = async ({ params }) => {
+  // console.log('abc: ', abc.params.eventId);
+  const {eventId: id} = params;
+
+  // use 로 시작하는 함수인 리액트훅은 컴포넌트 내부에서만 사용가능 ->  id, setEv 사용 불가
+  const response = await fetch(`http://localhost:8282/events/${id}`);
+
+  if(!response.ok) {
+    // 예외처리
+  }
+
+  return await response.json();
+
+};
