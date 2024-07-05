34f7c756c8f7d0291bca66c14da32c6cca8a8b49
dazzlingtea <kitsnr08@gmail.com>
2024-07-05 12:34:24 +0900

    # 리액트-서버 연동 : 무한스크롤 조건 추가

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,22 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import {useLoaderData, json} from "react-router-dom";
-import EventList from "../components/EventList";
-import EventSkeleton from "../components/EventSkeleton";
+import React, { useEffect, useState } from 'react';
+import EventList from '../components/EventList';
+import EventSkeleton from '../components/EventSkeleton';
 
 // npm install loadsh
-import {throttle} from "lodash";
-
+import { debounce, throttle } from 'lodash';
 
 const Events = () => {
 
   // loader가 리턴한 데이터 받아오기
   // const eventList = useLoaderData();
+  // console.log(eventList);
 
-  // console.log('loader 리턴 : ', eventList);
 
-  // 무한스크롤 - loader 대신 기존 방법대로
   // 서버에서 가져온 이벤트 목록
-  const [ events, setEvents] = useState([]);
+  const [events, setEvents] = useState([]);
 
   // 로딩 상태 체크
   const [loading, setLoading] = useState(false);
@@ -28,32 +25,34 @@ const Events = () => {
   const [isFinish, setIsFinish] = useState(false);
 
 
-  // 서버로 목록 조회 요청 보내기
-  const loadEvents = async()=> {
-
-    if(isFinish) {
+  // 서버로 목록 조회 요청보내기
+  const loadEvents = async() => {
+    console.log('loadEvents: ', events, loading,  currentPage, isFinish);
+    if (isFinish) {
       console.log('loading finished!');
       return;
     }
 
-    console.log('start loading... ');
+    console.log('start loading...');
     setLoading(true);
 
     const response = await fetch(`http://localhost:8282/events/page/${currentPage}?sort=date`);
-    const { events:loadedEvents, totalCount } = await response.json();
+    const { events: loadedEvents, totalCount } = await response.json();
 
-    const updatedEvents = [...events, ...loadedEvents];
+    // console.log('loaded: ', loadedEvents);
+
+    const updatedEvents = [...events, ...loadedEvents ];
+    console.log(updatedEvents);
     setEvents(updatedEvents);
-    setLoading(false); // fetch 종료시 스켈레톤 안보이게 처리
-
+    setLoading(false);
     // 로딩이 끝나면 페이지번호를 1 늘려놓는다.
+    setCurrentPage(prevPage => prevPage + 1);
     console.log('end loading!!');
-    setCurrentPage(prevPage => prevPage+1);
+    console.log('loadEvents: ', events, loading,  currentPage, isFinish);
 
-    // 로딩이 끝나면 더 가져올 게 있는지 여부를 확인한다.
+    // 로딩이 끝나면 더 이상 가져올게 있는지 여부를 체크한다.
     setIsFinish(totalCount === updatedEvents.length);
-
-  }
+  };
 
   // 초기 이벤트 1페이지 목록 가져오기
   useEffect(() => {
@@ -62,29 +61,28 @@ const Events = () => {
 
   // 스크롤 핸들러
   const scrollHandler = throttle(() => {
-    if(loading ||
-      window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeigh
+    if (loading ||
+      window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight
     ) {
       return;
     }
-
+    loadEvents();
   }, 2000);
 
   // 스크롤 이벤트 바인딩
   useEffect(() => {
-    window.addEventListener('scroll', e=>scrollHandler);
+    window.addEventListener('scroll', scrollHandler);
 
     return () => {
       window.removeEventListener('scroll', scrollHandler);
-      scrollHandler.cancel(); // 쓰로틀 취소
+      scrollHandler.cancel(); // 스로틀 취소
     }
   }, [currentPage, loading]);
-  // 페이지번호나 로딩상태가 바뀌면 스크롤이벤트 취소하고 다시 걸도록 설정
 
   return (
     <>
       <EventList eventList={events} />
-      {loading && <EventSkeleton/>}
+      {loading && <EventSkeleton />}
     </>
   );
 };
@@ -93,21 +91,17 @@ export default Events;
 
 // loader를 app.js로부터 아웃소싱
 // export const loader = async () => {
-//
-//   const response = await fetch('http://localhost:8282/events/page/3?sort=date');
-//
-//   if(!response.ok) {
+
+//   const response = await fetch('http://localhost:8282/events/page/1?sort=date');
+
+//   if (!response.ok) {
 //     const errorText = await response.text();
-//
-//     // throw new Response(JSON.stringify({ message: errorText }),
-//     throw json(     // router의 json 함수를 사용하면 바로 에러 객체 보낼 수 있음
+//     throw json(
 //       { message: errorText },
 //       {
 //         status: response.status
 //       }
 //     );
-//     // return response; // body error message ex) sort가 없습니다 가 들어있음
 //   }
-//
 //   return response; // ok일 경우 events[]
 // };
