b07d2d5a300a5da618aafed46e3e41639676c0b4
dazzlingtea <kitsnr08@gmail.com>
2024-07-05 11:01:45 +0900

    # 리액트-서버 연동 : 페이징 처리를 위한 url 변경

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -23,7 +23,7 @@ export default Events;
 // loader를 app.js로부터 아웃소싱
 export const loader = async () => {
 
-  const response = await fetch('http://localhost:8282/events?sort=date');
+  const response = await fetch('http://localhost:8282/events/page/3?sort=date');
 
   if(!response.ok) {
     const errorText = await response.text();
