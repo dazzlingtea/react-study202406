b1e5ea083a3af6af149bcf169a0169a19dc10683
dazzlingtea <kitsnr08@gmail.com>
2024-07-05 09:59:11 +0900

    # 리액트-서버 연동 : 삭제

--- src/components/RouteExample/components/EventItem.js
+++ src/components/RouteExample/components/EventItem.js
@@ -1,15 +1,43 @@
 import React from "react";
 import styles from './EventItem.module.scss';
-import {Link} from "react-router-dom";
+import {Link, redirect, useNavigate} from "react-router-dom";
 
 const EventItem = ({ event }) => {
 
   const {
+    'event-id': id,
     title,
     desc: description,
     'img-url':image,
     'start-date':date
   } = event;
+
+  const navigate = useNavigate();
+
+  // const fetchDelete = async () => {
+  //   const res = await fetch(`http://localhost:8282/events/${id}`, {
+  //     method: 'DELETE'
+  //   });
+  // }
+
+  const deleteHandler =  (e) => {
+    // EventDetail(여기에 Pathvariable로 id 존재) -> EventItem
+    // console.log('id: ', id)
+    if(!window.confirm('정말 삭제하시겠습니까?')) return;
+
+    // fetchDelete();
+
+    (async() => {
+      await fetch(`http://localhost:8282/events/${id}`, {
+        method: 'DELETE'
+      });
+    })();
+
+    setTimeout(() => {
+      navigate('/events');
+    }, 200);
+
+  };
 
   return (
     <article className={styles.event}>
@@ -19,7 +47,7 @@ const EventItem = ({ event }) => {
       <p>{description}</p>
       <menu className={styles.actions}>
         <Link to="edit">Edit</Link>
-        <button>Delete</button>
+        <button onClick={deleteHandler}>Delete</button>
       </menu>
     </article>
   );
